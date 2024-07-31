91974a5b0d5231316eb8d76c2bf774fb98a0b830
IslamRamadan1 <106744372+IslamRamadan1@users.noreply.github.com>
2024-08-01 01:59:42 +0300

    generate image

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Storage;
 
 class ImageService
@@ -13,8 +14,14 @@ class ImageService
         $imageName = uniqid('image_') . '.jpg';
         $imagePath = $folder_name . '/' . $imageName; // Path within the public/images folder
 
-        Storage::disk('s3')->put($imagePath, $image);
+//        Storage::disk('s3')->put($imagePath, $image);
+        Storage::disk('public')->put($imagePath, $image);
         return $imageName;
+    }
+
+    public function generate_image_link($image_path)
+    {
+        return Storage::disk('s3')->temporaryUrl($image_path, Carbon::now()->addMinutes(60));
     }
 
     public function delete_image(mixed $image_title, $folder_name)
