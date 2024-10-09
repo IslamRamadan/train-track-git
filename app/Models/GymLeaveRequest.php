565f8347677c20126dd33a8809c38b4dc06e871f
IslamRamadan1 <106744372+IslamRamadan1@users.noreply.github.com>
2024-10-09 17:01:27 +0300

    add database files

---
+++ app/Models/GymLeaveRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GymLeaveRequest extends Model
+{
+    use HasFactory;
+}
