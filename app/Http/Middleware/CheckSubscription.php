a270282cdf40764cf5cf823e710dcb781fdb6a4b
IslamRamadan <106744372+IslamRamadan1@users.noreply.github.com>
2025-01-20 10:38:20 +0200

    Update CheckSubscription.php

--- app/Http/Middleware/CheckSubscription.php
+++ app/Http/Middleware/CheckSubscription.php
@@ -33,7 +33,7 @@ class CheckSubscription
             "ip" => $request->ip(),
             "user_agent" => $request->header('User-Agent'),
             "route" => $request->getPathInfo(),
-            "body" => $request->has('img') ? null : $request->getContent(),
+            "body" => $request->has('img') || $request->has('image') ? null : $request->getContent(),
         ]);
 
         if ($request->user()->user_type == "0") {
