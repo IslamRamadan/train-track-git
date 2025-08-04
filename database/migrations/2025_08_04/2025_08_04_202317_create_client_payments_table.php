cd1d327a13317e0a5a667543493cfb3d9e8a4f84
IslamRamadan <106744372+IslamRamadan1@users.noreply.github.com>
2025-08-04 20:37:06 +0300

    add client payments table

---
+++ database/migrations/2025_08_04/2025_08_04_202317_create_client_payments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('client_payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('client_id')->nullable();
+            $table->foreign('client_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('order_id', 191);
+            $table->enum('status', ['0', '1', '2'])->default('1');
+            $table->integer('amount')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('client_payments');
+    }
+};
